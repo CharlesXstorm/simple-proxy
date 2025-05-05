19c614874d77a5254b14a61cd5c656ebd263eb6b
Charles Ukadike <charlesukadike44@gmail.com>
2025-05-05 18:19:02 +0100

    Update nitro.config.ts

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,7 +3,7 @@ import pkg from "./package.json";
 
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
-  noPublicDir: true,
+  noPublicDir: false,
   srcDir: "./src",
   runtimeConfig: {
     version: pkg.version
